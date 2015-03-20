bda7849fb42f3d24956fdc3cbe4d9f4782897ed3
Kevin Keller <flygeneticist@gmail.com>
2015-03-20 15:14:17 +0000

    removal of expenses and destinations add + cities and countries tables added + other misc changes.

--- nomad-planner.js
+++ nomad-planner.js
@@ -2,12 +2,13 @@
 Destinations = new Mongo.Collection("destinations");
 Expenses = new Mongo.Collection("expenses");
 Statistics = new Mongo.Collection("statistics");
-
+Countries = new Mongo.Collection("countries");
+Cities = new Mongo.Collection("cities");
 
 if (Meteor.isClient) {
   Template.body.helpers({
     destinations: function () {
-      return Destinations.find({}, {sort: {dateStart: -1}});
+      return Destinations.find({}, {sort: {dateStart: 1}});
     },
     expenses: function () {
       return Expenses.find({});
@@ -22,5 +23,71 @@ if (Meteor.isClient) {
     //   // increment the counter when button is clicked
     //   Session.set('counter', Session.get('counter') + 1);
     // }
+    'onchange #country': function () {
+      console.log(event.target.country.value);
+    },
+
+    'submit #destForm': function (event) {
+      var country = event.target.country.value;
+      var city = event.target.city.value;
+      var budget = event.target.budget.value;
+      var dateStart = new Date(event.target.dateStart.value);
+      var dateEnd = new Date(event.target.dateEnd.value);
+
+      addDestination(country, city, budget, dateStart, dateEnd);
+      var cost = calcBudgetCost(budget, parseInt(city));
+      addExpense(country, cost);
+
+      event.target.country.value = "";
+      event.target.city.value = "";
+      event.target.budget.value = "";
+      event.target.dateStart.value = "";
+      event.target.dateEnd.value = "";
+      return false;
+    },
+
+    'click #delDest': function () {
+      Destinations.remove(this._id);
+    },
+
+    'click #delExpen': function () {
+      Expenses.remove(this._id);
+    }
   });
+}
+
+
+function addDestination (ctry, city, budget, dateStart, dateEnd) {
+  Destinations.insert({
+    country: ctry,
+    city: city,
+    budget: budget,
+    dateStart: dateStart,
+    dateEnd: dateEnd,
+    duration: Math.floor(dateEnd - dateStart) / (1000*60*60*24),
+    createdAt: new Date()
+  });
+}
+
+function addExpense (ctry, cost, category) {
+  if (typeof category === undefined) {
+    category = 'base-costs';
+  }
+
+  Expenses.insert({
+    tripLeg: 'placeholder',
+    title: country + ' Cost of Living',
+    category: category,
+    cost: cost
+  });
+}
+
+function calcBudgetCost (budgetLvl, cityId) {
+  var cost = Cities.find({id: cityId})[0];
+  if (budgetLvl == 'high') {
+    cost = cost["costHigh"];
+  } else {
+    cost = cost["costLow"];
+  }
+  return cost;
 }

5a59170408616ad4b8a3d22cc6643e9c98f5f8c8
Kevin Keller <flygeneticist@gmail.com>
2015-03-20 17:16:35 +0000

    Frontend UI helpers added, custom expenses working now, and moment package added.

--- nomad-planner.js
+++ nomad-planner.js
@@ -27,26 +27,41 @@ if (Meteor.isClient) {
     //   // increment the counter when button is clicked
     //   Session.set('counter', Session.get('counter') + 1);
     // }
-    'onchange #country': function () {
-      console.log(event.target.country.value);
-    },
+    // 'onchange #country': function () {
+    //   console.log(event.target.country.value);
+    // },
 
     'submit #destForm': function (event) {
-      var country = event.target.country.value;
-      var city = event.target.city.value;
+      var country = parseInt(event.target.country.value);
+      var city = parseInt(event.target.city.value);
       var budget = event.target.budget.value;
       var dateStart = new Date(event.target.dateStart.value);
       var dateEnd = new Date(event.target.dateEnd.value);
 
       addDestination(country, city, budget, dateStart, dateEnd);
-      var cost = calcBudgetCost(budget, parseInt(city));
-      addExpense(country, cost);
+      var cost = calcBudgetCost(budget, city);
+      addExpense(city, cost);
 
       event.target.country.value = "";
       event.target.city.value = "";
       event.target.budget.value = "";
       event.target.dateStart.value = "";
       event.target.dateEnd.value = "";
+      return false;
+    },
+
+    'submit #expenForm': function (event) {
+      var city = undefined;
+      var cost = parseFloat(event.target.cost.value).toFixed(2);
+      var category = event.target.category.value;
+      var title = event.target.title.value;
+
+      addExpense(city, cost, category, title);
+
+      event.target.city.value = "";
+      event.target.cost.value = "";
+      event.target.category.value = "";
+      event.target.title.value = "";
       return false;
     },
 
@@ -73,21 +88,19 @@ function addDestination (ctry, city, budget, dateStart, dateEnd) {
   });
 }
 
-function addExpense (ctry, cost, category) {
-  if (typeof category === undefined) {
-    category = 'base-costs';
-  }
-
+function addExpense (city, cost, category, title) {
+  if (category === undefined) { category = 'base-costs'; }
+  if (title === undefined) { title = Cities.findOne({id: city})['name']; }
   Expenses.insert({
     tripLeg: 'placeholder',
-    title: country + ' Cost of Living',
+    title: title,
     category: category,
     cost: cost
   });
 }
 
 function calcBudgetCost (budgetLvl, cityId) {
-  var cost = Cities.find({id: cityId})[0];
+  var cost = Cities.findOne({id: cityId});
   if (budgetLvl == 'high') {
     cost = cost["costHigh"];
   } else {
@@ -95,3 +108,28 @@ function calcBudgetCost (budgetLvl, cityId) {
   }
   return cost;
 }
+
+// Helper Functions Defined Below This Point
+var DateFormats = {
+       short: "MMMM DD, YYYY",
+       long: "dddd MM.DD.YYYY HH:mm"
+};
+
+UI.registerHelper("formatDate", function (datetime, format) {
+  if (moment) {
+    // can use other formats like 'lll' too
+    format = DateFormats[format] || format;
+    return moment(datetime).format(format);
+  }
+  else {
+    return datetime;
+  }
+});
+
+UI.registerHelper("getCountryName", function (ctryId) {
+  return Countries.findOne({id: ctryId})['name'];
+});
+
+UI.registerHelper("getCityName", function (ctyId) {
+  return Cities.findOne({id: ctyId})['name'];
+});

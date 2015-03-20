dc7acb20e7af4f3cbb0bc00de10f173360789c83
Kevin Keller <flygeneticist@gmail.com>
2015-03-20 13:22:46 +0000

    Test data added to mongo collections.

--- nomad-planner.js
+++ nomad-planner.js
@@ -1,32 +1,26 @@
 // setup Mongo collection
 Destinations = new Mongo.Collection("destinations");
 Expenses = new Mongo.Collection("expenses");
-Stats = new Mongo.Collection("stats");
+Statistics = new Mongo.Collection("statistics");
 
 
 if (Meteor.isClient) {
   Template.body.helpers({
-    destination: function () {
+    destinations: function () {
       return Destinations.find({}, {sort: {dateStart: -1}});
     },
-    expense: function () {
-      return Expenses.find({}, {sort: {createdAt: -1}});
+    expenses: function () {
+      return Expenses.find({});
     },
-    stats: function () {
-      return Destinations.find({});
+    statistics: function () {
+      return Statistics.find({});
     }
   });
 
   Template.body.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
-}
-
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
+    // 'click button': function () {
+    //   // increment the counter when button is clicked
+    //   Session.set('counter', Session.get('counter') + 1);
+    // }
   });
 }

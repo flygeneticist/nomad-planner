6d0a3c45407bf33326ce01dd954f4d4103e3ea22
Kevin Keller <flygeneticist@gmail.com>
2015-03-20 12:49:02 +0000

    Mongo collections added + minor front-end tweaks.

--- nomad-planner.js
+++ nomad-planner.js
@@ -1,14 +1,23 @@
-if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
+// setup Mongo collection
+Destinations = new Mongo.Collection("destinations");
+Expenses = new Mongo.Collection("expenses");
+Stats = new Mongo.Collection("stats");
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
+
+if (Meteor.isClient) {
+  Template.body.helpers({
+    destination: function () {
+      return Destinations.find({}, {sort: {dateStart: -1}});
+    },
+    expense: function () {
+      return Expenses.find({}, {sort: {createdAt: -1}});
+    },
+    stats: function () {
+      return Destinations.find({});
     }
   });
 
-  Template.hello.events({
+  Template.body.events({
     'click button': function () {
       // increment the counter when button is clicked
       Session.set('counter', Session.get('counter') + 1);
